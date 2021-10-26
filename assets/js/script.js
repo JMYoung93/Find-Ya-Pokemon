b752de255d83791425fe7216dd390e63361a9f24
Jeffrey <hernew@gmail.com>
2021-10-25 21:30:39 -0700

    gave id to li in selected team cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -148,6 +148,7 @@ function addToTeam(){
         var cardImgEl = document.createElement("img")
         var imgSource = data.sprites.front_default
         var imgItemEl = document.createElement("li")
+        imgItemEl.setAttribute("id", "pokeCard")
         cardImgEl.setAttribute('src', imgSource)
         cardEl.append(cardImgEl)
         imgItemEl.append(cardEl)
