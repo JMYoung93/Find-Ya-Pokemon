fdcd3a9f3d2c531f41fd04c246450527a8e25074
Jeffrey <hernew@gmail.com>
2021-10-25 21:32:27 -0700

    changed card id for selected pokemon

--- assets/js/script.js
+++ assets/js/script.js
@@ -148,7 +148,7 @@ function addToTeam(){
         var cardImgEl = document.createElement("img")
         var imgSource = data.sprites.front_default
         var imgItemEl = document.createElement("li")
-        imgItemEl.setAttribute("id", "pokeCard")
+        cardEl.setAttribute("id", "pokeCard")
         cardImgEl.setAttribute('src', imgSource)
         cardEl.append(cardImgEl)
         imgItemEl.append(cardEl)
