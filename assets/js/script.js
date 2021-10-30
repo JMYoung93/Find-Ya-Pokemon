5e650e71a638802e39354699b0077a6087da2312
cody elm <codyelm@gmail.com>
2021-10-30 10:35:45 -0700

    styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -195,8 +195,9 @@ function addToTeam(){
         cardEl.append(attackList)
 
         var removeBtn = document.createElement('button')
-        removeBtn.textContent = "Remove"
+        //removeBtn.textContent = "Remove"
         removeBtn.setAttribute('pokemonName', data.name)
+        removeBtn.setAttribute("class","delete is-medium")
         removeBtn.addEventListener('click', function(event){
         console.log('Remove Success')
         var deleteThisPokemon = event.target.getAttribute("pokemonName")
@@ -247,7 +248,7 @@ function makeTeam(){
     for(var i = 0; i < cardTeamHistory.length; i++){
         var newCardEl = document.createElement('div')
         newCardEl.setAttribute('class', 'card')
-
+        newCardEl.setAttribute('id', 'pokeCard')
         var newCardItemEl = document.createElement('li')
 
         var newNameEl = document.createElement('h2')
@@ -278,8 +279,9 @@ function makeTeam(){
         savedAttackList.append(newAttackLi4)
 
         var removeBtn = document.createElement('button')
-        removeBtn.textContent = "Remove"
+        //removeBtn.textContent = "Remove"
         removeBtn.setAttribute("pokemonName", cardTeamHistory[i].name)
+        removeBtn.setAttribute("class","delete is-medium")
         removeBtn.addEventListener('click', function(event){
             //get the pokemon name 
             var deleteThisPokemon = event.target.getAttribute("pokemonName")
@@ -349,6 +351,7 @@ function makeButtons() {
             if(searchedPokemon){
                 newBtnEl.textContent = searchedPokemon
                 newBtnEl.setAttribute('id', 'historyBtn')
+                newBtnEl.setAttribute('class', "button is-fullwidth")
             
         
                 // listItemEl.append(newBtnEl);
